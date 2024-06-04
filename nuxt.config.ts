26ff10fe9d232edb4d31e7bddefe70178fab877a
wermoss <konrad@MacBook-Air-Konrad.local>
2024-06-04 15:29:10 +0200

    SEO Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { repositoryName, apiEndpoint } from './slicemachine.config.json';
+import { repositoryName, apiEndpoint } from "./slicemachine.config.json";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -6,37 +6,41 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Prismic + Nuxt Minimal Starter',
+      title: "Prismic + Nuxt Minimal Starter",
       htmlAttrs: {
-        lang: 'en',
+        lang: "en",
       },
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    }
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
   },
 
-  modules: ['@nuxtjs/prismic'],
+  modules: ["@nuxtjs/prismic"],
 
   prismic: {
     endpoint: apiEndpoint || repositoryName,
-    preview: '/api/preview',
+    preview: "/api/preview",
     clientConfig: {
       routes: [
         {
-          type: 'page',
-          path: '/:uid',
+          type: "page",
+          path: "/:uid",
         },
         {
-          type: 'page',
-          uid: 'home',
-          path: '/',
+          type: "page",
+          uid: "home",
+          path: "/",
         },
-      ]
-    }
+        {
+          type: "article",
+          path: "/articles/:uid",
+        },
+      ],
+    },
   },
-})
+});
