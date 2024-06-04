c205e33810f09cb6701e64f5de86d258b80ad8b0
wermoss <konrad@MacBook-Air-Konrad.local>
2024-06-04 15:37:40 +0200

    Seo if

--- pages/articles/[uid].vue
+++ pages/articles/[uid].vue
@@ -14,14 +14,16 @@ const { data: settings } = useAsyncData("settings", () =>
 );
 
 watchEffect(() => {
-  useSeoMeta({
-    title: page.value?.data.meta_title ?? settings.value?.data.site_title,
-    description:
-      page.value?.data.meta_description ??
-      settings.value?.data.meta_description,
-    ogImage:
-      page.value?.data.meta_image?.url ?? settings.value?.data.og_image.url,
-  });
+  if (page.value?.data) {
+    useSeoMeta({
+      title: page.value?.data.meta_title ?? settings.value?.data.site_title,
+      description:
+        page.value?.data.meta_description ??
+        settings.value?.data.meta_description,
+      ogImage:
+        page.value?.data.meta_image?.url ?? settings.value?.data.og_image.url,
+    });
+  }
 });
 </script>
 
