e7e26409af20afddf7f730894993c27a72a9440a
wermoss <konrad@MacBook-Air-Konrad.local>
2024-06-14 20:46:30 +0200

    bottom form

--- components/FormFooter.vue
+++ components/FormFooter.vue
@@ -2,9 +2,15 @@
   <div class="text-3xl pb-2">{{ sectionTitle }}</div>
   <span class="block bg-[#FFCB04] h-[2px] w-20 my-2"></span>
 
-  <div class="pt-2 pb-10">{{ sectionSubtitle }}</div>
-  <div class="bg-gray-100">
-    <form id="contactForm" @submit.prevent="sendEmail" class="tracking-wide">
+  <div class="pt-2 pb-10">
+    {{ sectionSubtitle }}
+  </div>
+  <div class="rounded">
+    <form
+      id="contactForm"
+      @submit.prevent="sendEmailAndSMS"
+      class="tracking-wide"
+    >
       <label for="name" class="text-sm">Imię i nazwisko</label><br />
       <input
         type="text"
@@ -20,15 +26,20 @@
         name="company"
         v-model="company"
         class="w-full mt-2 mb-2 px-4 py-2 outline-none border-[1px] border-[transparent] focus:border-[1px] focus:border-[#000000]"
-      /><br />
-      <label for="phone" class="text-sm">Telefon</label><br />
-      <input
-        type="text"
-        id="phone"
-        name="phone"
-        v-model="phone"
-        class="w-full mt-2 mb-2 px-4 py-2 outline-none border-[1px] border-[transparent] focus:border-[1px] focus:border-[#000000]"
-      /><br />
+      /><br /><label for="phone" class="text-sm">Telefon</label><br />
+      <div
+        class="flex items-center border border-transparent focus-within:border-black"
+      >
+        <span class="bg-white pl-4 py-2">+48</span>
+        <input
+          type="text"
+          id="phone"
+          name="phone"
+          maxlength="9"
+          v-model="userPhone"
+          class="w-full pl-2 py-2 outline-none border-transparent focus:border-black"
+        />
+      </div>
       <label for="email" class="text-sm">Adres email</label><br />
       <input
         type="email"
@@ -86,7 +97,6 @@
     </form>
   </div>
 </template>
-
 <script setup lang="ts">
 import { ref, onMounted, computed } from "vue";
 
@@ -109,27 +119,38 @@ const apiKey = config.public.brevoApiKey;
 const emailStatus = ref("");
 const name = ref("");
 const company = ref("");
-const phone = ref("");
+const userPhone = ref(""); // Zmieniono nazwę zmiennej z 'phone' na 'userPhone'
 const email = ref("");
 const message = ref("");
 const isChecked = ref(false);
+const sendSMSAllowed = ref(true);
 
 const isFormValid = computed(() => {
   return (
     name.value &&
     company.value &&
+    userPhone.value && // Zmieniono nazwę zmiennej z 'phone' na 'userPhone'
     email.value &&
     message.value &&
     isChecked.value
   );
 });
 
-const sendEmail = async () => {
+const ensurePrefix = () => {
+  userPhone.value = userPhone.value.replace(/\D/g, ""); // Remove non-digit characters
+  if (!userPhone.value.startsWith("48")) {
+    userPhone.value = "48" + userPhone.value;
+  }
+};
+
+const sendEmailAndSMS = async () => {
   if (!isFormValid.value) {
     return;
   }
+  ensurePrefix(); // Ensure the phone number is correctly formatted
+
   try {
-    const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+    let response = await fetch("https://api.brevo.com/v3/smtp/email", {
       method: "POST",
       headers: {
         accept: "application/json",
@@ -138,49 +159,128 @@ const sendEmail = async () => {
       },
       body: JSON.stringify({
         sender: {
-          name: "Wooboo.pl",
-          email: "konrad@wooboo.pl",
+          name: "Benchmarket.pl",
+          email: "services@benchmarket.pl",
         },
         replyTo: {
-          email: "konrad@wooboo.pl",
-          name: "Wooboo.pl",
+          name: "Benchmarket.pl",
+          email: "services@benchmarket.pl",
         },
-        templateId: 5,
+        templateId: 1,
         params: {
           FORM_NAME: `${name.value}`,
           FORM_COMPANY: `${company.value}`,
+          FORM_PHONE: `${userPhone.value}`, // Zmieniono nazwę zmiennej z 'phone' na 'userPhone'
           FORM_EMAIL: `${email.value}`,
           FORM_MESSAGE: `${message.value}`,
         },
         to: [
           {
-            email: "konrad@wooboo.pl",
-            name: `${name.value}`,
-          },
-          {
-            email: "michal.olejniczak@benchmarket.pl",
-            name: `${name.value}`,
+            name: "Benchmarket.pl",
+            email: "services@benchmarket.pl",
           },
         ],
-        subject: "Nowy furmularz kontaktowy",
-        htmlContent: `<html><head></head><body><p>Imię i nazwisko: ${name.value}</p><p>${message.value}</p><p>Nazwa firmy: ${company.value}</p><p>Email: ${email.value}</p></body></html>`,
-        textContent: "Nowy furmularz kontaktowy",
+        cc: [
+          {
+            name: "Michal Olejniczak",
+            email: "michal.olejniczak@benchmarket.pl",
+          },
+        ],
+        subject: "Nowa wiadomość ze strony internetowej",
       }),
     });
-    console.log(await response.text());
+
     if (!response.ok) {
       throw new Error("Email sending failed");
     }
+
+    console.log("Email sent successfully to", "services@benchmarket.pl");
+
+    // Second email sending block
+    await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "api-key": apiKey,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        sender: {
+          name: "Benchmarket.pl",
+          email: "services@benchmarket.pl",
+        },
+        replyTo: {
+          name: "Benchmarket.pl",
+          email: "services@benchmarket.pl",
+        },
+        templateId: 3, // Use template mr 3 for the second email
+        to: [
+          {
+            name: name.value, // Use the name from the form
+            email: email.value, // Use the email from the form
+          },
+        ],
+        subject: "Potwierdzenie otrzymania wiadomości",
+      }),
+    });
+
+    console.log("Email sent successfully to", email.value);
+
+    if (sendSMSAllowed.value) {
+      await sendSMS(
+        userPhone.value, // Use 'userPhone.value' for the phone number
+        "Dziekujemy za kontakt. Skontaktujemy sie tak szybko jak to tylko mozliwe. Zespol Doradcow Podatkowych / Benchmarket.pl "
+      );
+      // console.log("SMS sent successfully to", userPhone.value); // Corrected to use 'userPhone.value'
+
+      await sendSMS(
+        "48660440456",
+        "Panowie, wlasnie wypelniono formularz na stronie internetowej. Warto zajrzec na maila :)"
+      );
+      // console.log("SMS sent successfully to", "48450066055");
+    }
+
     emailStatus.value = "success";
-    // Clear the form fields
     name.value = "";
     company.value = "";
+    userPhone.value = ""; // Zmieniono nazwę zmiennej z 'phone' na 'userPhone'
     email.value = "";
     message.value = "";
     isChecked.value = false;
   } catch (error) {
     console.error(error);
     emailStatus.value = "error";
+  }
+};
+
+const sendSMS = async (recipient, content) => {
+  try {
+    const response = await fetch(
+      "https://api.brevo.com/v3/transactionalSMS/sms",
+      {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "api-key": apiKey,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "transactional",
+          unicodeEnabled: false,
+          sender: "Benchmarket",
+          recipient: recipient,
+          content: content,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("SMS sending failed");
+    }
+
+    console.log("SMS sent successfully to", recipient);
+  } catch (error) {
+    console.error("Failed to send SMS to", recipient, error);
   }
 };
 </script>
